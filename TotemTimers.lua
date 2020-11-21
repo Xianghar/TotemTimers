b057a4c5ec2978744dc1991c3a2a5fbde6fe2a24
Xianghar <darkone02@live.de>
2020-11-21 23:21:20 +0100

    Show totem duration on buttons

--- TotemTimers.lua
+++ TotemTimers.lua
@@ -17,6 +17,27 @@ if select(2,UnitClass("player")) ~= "SHAMAN" then return end
 
 local _, TotemTimers = ...
 
+local TotemSlotTimers = {}
+
+local TotemTimersOnEvent = function(self, event, slot, ...)
+	if (event == "PLAYER_TOTEM_UPDATE") then
+		local _,_,startTime, duration, icon = GetTotemInfo(slot)
+		if startTime then
+			local timer = XiTimers.TextureToTimer[icon]
+			if (timer and timer.active) then
+				TotemSlotTimers[slot] = timer
+				timer:Start(startTime, startTime + duration, true)
+			end
+		else
+			if (timer and timer.active and timer.running and timer.buffIsActive) then
+				timer:stop()
+			end
+			TotemSlotTimers[slot] = nil
+		end
+	else
+		XiTimers.OnEvent(self, event, slot, ...)
+	end
+end
 
 function XiTimers.CustomInit()
     XiTimers_Profiles = TotemTimers_Profiles
@@ -26,4 +47,8 @@ function XiTimers.CustomInit()
 	SLASH_TOTEMTIMERS1 = "/totemtimers";
 	SLASH_TOTEMTIMERS2 = "/tt";
 	SlashCmdList["TOTEMTIMERS"] = XiTimers.Slash
+
+
+	XiTimersFrame:RegisterEvent("PLAYER_TOTEM_UPDATE")
+	XiTimersFrame:SetScript("OnEvent", TotemTimersOnEvent)
 end
